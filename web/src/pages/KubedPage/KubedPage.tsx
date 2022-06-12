39f29065b3d0ab3000f8b35daaa300793bafbffc
Jeremy Evans <jeremyevans@Jeremys-MacBook-Pro.local>
2022-06-11 23:46:15 -0400

    color

--- web/src/pages/KubedPage/KubedPage.tsx
+++ web/src/pages/KubedPage/KubedPage.tsx
@@ -7,13 +7,15 @@ const KubedPage = () => {
       <MetaTags title="Kubed" description="Kubed page" />
 
       <h1>KubedPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/KubedPage/KubedPage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>kubed</code>, link to me with `
-        <Link to={routes.kubed()}>Kubed</Link>`
-      </p>
+      <div className="grid">
+        <p>
+          Find me in <code>./web/src/pages/KubedPage/KubedPage.tsx</code>
+        </p>
+        <p>
+          My default route is named <code>kubed</code>, link to me with `
+          <Link to={routes.kubed()}>Kubed</Link>`
+        </p>
+      </div>
     </>
   )
 }
